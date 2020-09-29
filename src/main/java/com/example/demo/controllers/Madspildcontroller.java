e7854694ab5ebea5355d17f954f1169af11ae860
AdamHviid <adam5596@stud.kea.dk>
2020-09-29 15:08:16 +0200

    added everything ;)

--- src/main/java/com/example/demo/controllers/Madspildcontroller.java
+++ src/main/java/com/example/demo/controllers/Madspildcontroller.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Lager;
 import com.example.demo.models.Opskrifter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +12,9 @@ import java.util.ArrayList;
 @Controller
 public class Madspildcontroller {
 
-
     ArrayList<String> listLager = new ArrayList<>();
+    ArrayList<String> koedsovs = new ArrayList<>();
+    ArrayList<String> burger = new ArrayList<>();
 
     @GetMapping("/")
     public String index(Model lagerModel) {
@@ -29,17 +29,31 @@ public class Madspildcontroller {
         return "redirect:/";
     }
 
+    @PostMapping("/removeLastFromLager")
+    public String removeLast(WebRequest formData) {
+        listLager.remove(listLager.size()-1);
+        return "redirect:/";
+    }
+
     @PostMapping("/opskrifter")
-    public String postOpskrifter(ArrayList listLager) {
-        Opskrifter koedsovs = new Opskrifter("hakkekød","pasta","tomat","","");
+    public String postOpskrifter(Model opskriftModel) {
+        koedsovs.add("Hakkekød");
+        koedsovs.add("Pasta");
+        koedsovs.add("Tomater");
+        burger.add("Hakkekød");
+        burger.add("Ost");
+        burger.add("Tomater");
 
-        for(Object i: listLager) {
-            if (i.equals(koedsovs.getIngrediens1())) {
-                return "pasta med kødsovs, mums";
+        for (Object i : listLager) {
+            if (listLager.contains("Hakkekød") || listLager.contains("Pasta") || listLager.contains("Tomater")) {
+                opskriftModel.addAttribute("kødsovs", koedsovs);
+                return "opskrifter";
             }
-            return "ups";
+            if (listLager.contains("Hakkekød") || listLager.contains("Ost") || listLager.contains("Tomater")) {
+                return "opskrifter";
+            }
+            return "redirect:/";
         }
-        return "ups";
-
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/demo/models/Opskrifter.java
+++ src/main/java/com/example/demo/models/Opskrifter.java
@@ -1,38 +1,29 @@
+
 package com.example.demo.models;
+
+import java.util.ArrayList;
 
 public class Opskrifter {
 
-    private String ingrediens1;
-    private String ingrediens2;
-    private String ingrediens3;
-    private String ingrediens4;
-    private String ingrediens5;
+    private String ingrediens;
 
-    public Opskrifter(String ingrediens1, String ingrediens2, String ingrediens3, String ingrediens4, String ingrediens5) {
-        this.ingrediens1 = ingrediens1;
-        this.ingrediens2 = ingrediens2;
-        this.ingrediens3 = ingrediens3;
-        this.ingrediens4 = ingrediens4;
-        this.ingrediens5 = ingrediens5;
+    public String getIngrediens() {
+        return ingrediens;
     }
 
-    public String getIngrediens1() {
-        return ingrediens1;
+    public void setIngrediens(String ingrediens) {
+        this.ingrediens = ingrediens;
     }
 
-    public String getIngrediens2() {
-        return ingrediens2;
-    }
-
-    public String getIngrediens3() {
-        return ingrediens3;
-    }
-
-    public String getIngrediens4() {
-        return ingrediens4;
-    }
-
-    public String getIngrediens5() {
-        return ingrediens5;
+    public Opskrifter(String ingrediens) {
+        this.ingrediens = ingrediens;
     }
 }
+/*
+    public Object koedsovs() {
+
+        return koedsovs;
+    }
+
+}
+*/
